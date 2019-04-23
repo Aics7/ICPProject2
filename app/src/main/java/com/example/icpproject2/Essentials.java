bf0c95547ff6371b10eeaa3a9bd16a85d307e13b
Aics7 <issifu.alhassan@ashesi.edu.gh>
2019-04-23 00:41:34 +0000

    Initial commit

---
+++ app/src/main/java/com/example/icpproject2/Essentials.java
@@ -0,0 +1,13 @@
+package com.example.icpproject2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Essentials extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_essentials);
+    }
+}
--- app/src/main/java/com/example/icpproject2/MainActivity.java
+++ app/src/main/java/com/example/icpproject2/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.icpproject2;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.PopupMenu;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener{
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -31,6 +33,30 @@ public class MainActivity extends AppCompatActivity {
     public void getAllAccountData(View view){
         Intent int1=new Intent(MainActivity.this,AccountsData.class);
         startActivity(int1);
+    }
+    public void showStakeholders(View v) {
+        PopupMenu popup = new PopupMenu(this, v);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.menu_example, popup.getMenu());
+        popup.show();
+    }
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.zoomlion:
+                Intent int1=new Intent(MainActivity.this,Zoomlion.class);
+                startActivity(int1);
+            case R.id.essentials:
+                Intent int2=new Intent(MainActivity.this,Essentials.class);
+                startActivity(int2);
+            case R.id.bigben:
+                Intent int3=new Intent(MainActivity.this,Bigben.class);
+                startActivity(int3);
+            case R.id.akorno:
+                Intent int4=new Intent(MainActivity.this,Akorno.class);
+                startActivity(int4);
+        }
+        return true;
     }
 
 
