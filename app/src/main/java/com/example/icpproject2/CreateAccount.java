f6728ce0f8dc16f9f9dc38bc8dbb592a61908082
Faweenagua <46810972+Faweenagua@users.noreply.github.com>
2019-04-24 16:57:50 +0000

    added Firebase login and sign-up features

--- app/src/main/java/com/example/icpproject2/CreateAccount.java
+++ app/src/main/java/com/example/icpproject2/CreateAccount.java
@@ -1,6 +1,7 @@
 package com.example.icpproject2;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,56 +9,81 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class CreateAccount extends AppCompatActivity {
+
+
+    private EditText mail, password;
+    FirebaseAuth authUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        password = (EditText) findViewById(R.id.password);
+        mail = (EditText) findViewById(R.id.email);
+
+        authUser = FirebaseAuth.getInstance();
     }
 
     public void create(View view){
+        if(mail.getText().toString().equals("") && password.getText().toString().equals("")){
+            Toast.makeText(this, "Provide email & password", Toast.LENGTH_SHORT).show();
+        }else {
 
-        EditText edit1 = (EditText) findViewById(R.id.email);
-        String email = edit1.getText().toString();
-        EditText edit2 = (EditText) findViewById(R.id.password);
-        String password=edit2.getText().toString();
+            String eeemail = mail.getText().toString();
+            String passssword = password.getText().toString();
+            authUser.createUserWithEmailAndPassword(eeemail,passssword)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(CreateAccount.this, "Sign-up Complete", Toast.LENGTH_SHORT).show();
+                                finish();
 
-        if(MakePayment.accounts.indexOf(email)!=-1){
-            Toast.makeText(getApplicationContext(), "Account already Exists", Toast.LENGTH_LONG).show();
+                                Intent int1=new Intent(CreateAccount.this,MainActivity.class);
+                                startActivity(int1);
+                            }else {
+                                Toast.makeText(CreateAccount.this, "User Couldn't Sign-up", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(CreateAccount.this, "Try Again", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
 
-            Intent int1=new Intent(CreateAccount.this,MainActivity.class);
-            startActivity(int1);
-        }
-        else if(email.length()>=12 && password.length()>=8){
-            MakePayment.accounts.add(email);
-            MakePayment.password.add(password);
-            MakePayment.balance.add(0.00);
-            Toast.makeText(getApplicationContext(), "Account created Successfully", Toast.LENGTH_LONG).show();
-
-            Intent int1=new Intent(CreateAccount.this,MainActivity.class);
-            startActivity(int1);
-        }
-        else if(email.length()<10){
-            Toast.makeText(getApplicationContext(), "Email length is below standard", Toast.LENGTH_LONG).show();
-        }
-        else if(password.length()<8){
-            Toast.makeText(getApplicationContext(), "Password must be 8 characters or more", Toast.LENGTH_LONG).show();
-        }
-        else{
-            Toast.makeText(getApplicationContext(), "Both email and password are required to create an account", Toast.LENGTH_LONG).show();
-        }
     }
 
     public void login(View view) {
 
-        EditText edit1 = (EditText) findViewById(R.id.email);
-        String email = edit1.getText().toString();
-        EditText edit2 = (EditText) findViewById(R.id.password);
-        String password = edit2.getText().toString();
+        if(mail.getText().toString().equals("") && password.getText().toString().equals("")) {
+            Toast.makeText(this, "Provide email & password", Toast.LENGTH_SHORT).show();
+        }else {
 
-        Intent int1=new Intent(CreateAccount.this,MainActivity.class);
-        startActivity(int1);
+            String eeemial = mail.getText().toString();
+            String paaassword = password.getText().toString();
+            authUser.signInWithEmailAndPassword(eeemial,paaassword)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(CreateAccount.this, "Log-in Complete", Toast.LENGTH_SHORT).show();
+                                finish();
+                                // open new screen here
+                                Intent int1=new Intent(CreateAccount.this,MainActivity.class);
+                                startActivity(int1);
+                            }else {
+                                Toast.makeText(CreateAccount.this, "User Couldn't Log-in", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(CreateAccount.this, "Try Again", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    });
+        }
 
     }
 }
