ed5e613f0668da089cdfb67202d20034bca6fd40
Faweenagua <46810972+Faweenagua@users.noreply.github.com>
2019-04-29 00:33:08 +0000

    sfgrtdhgcwre

---
+++ app/src/main/java/com/example/icpproject2/Stakeholder.java
@@ -0,0 +1,40 @@
+package com.example.icpproject2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class Stakeholder extends AppCompatActivity {
+
+    public  static int index = 0;
+
+    public static ArrayList<String> stakeHolderAccounts =new ArrayList<String>();
+    public static ArrayList<String> stakeHolderAccountnames =new ArrayList<String>();
+    public static ArrayList<String> stakeHolderPassword =new ArrayList<String>();
+
+
+
+    TextView stakeHolderName;
+    Button showData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stake_holder);
+
+        stakeHolderName = (TextView) findViewById(R.id.stakeHolderName);
+
+        stakeHolderName.setText(stakeHolderAccountnames.get(index));
+
+    }
+
+    public void showUserData(View view){
+        Intent int1=new Intent(Stakeholder.this,AccountsData.class);
+        startActivity(int1);
+    }
+}
