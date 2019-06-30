f8fd569c23fc498fab2cdb259453a79a566d2e8a
Aics7 <issifu.alhassan@ashesi.edu.gh>
2019-06-30 18:07:42 +0000

    current

--- app/src/main/java/com/example/icpproject2/Zoomlion.java
+++ app/src/main/java/com/example/icpproject2/Zoomlion.java
@@ -1,8 +1,12 @@
 package com.example.icpproject2;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Zoomlion extends AppCompatActivity {
 
@@ -15,33 +19,56 @@ public class Zoomlion extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zoomlion);
 
-        if(MakePayment.akornoBinNum>=5){
-            TextView newTextView=(TextView) findViewById(R.id.akonum);
+        if (MakePayment.akornoBinNum >= 5) {
+            TextView newTextView = (TextView) findViewById(R.id.akonum);
             newTextView.setText("Full");
+            sendEmail("Akorno");
+        } else {
+            TextView newTextView = (TextView) findViewById(R.id.akonum);
+            newTextView.setText("" + MakePayment.akornoBinNum);
         }
-        else{
-            TextView newTextView=(TextView) findViewById(R.id.akonum);
-            newTextView.setText(""+MakePayment.akornoBinNum);
-        }
-        if(MakePayment.BigBenBinNum>=5){
-            TextView newTextView=(TextView) findViewById(R.id.bignum);
+        if (MakePayment.BigBenBinNum >= 5) {
+            TextView newTextView = (TextView) findViewById(R.id.bignum);
             newTextView.setText("Full");
-        }
-        else{
-            TextView newTextView1=(TextView) findViewById(R.id.bignum);
-            newTextView1.setText(""+MakePayment.BigBenBinNum);
+            sendEmail("BigBen");
+        } else {
+            TextView newTextView1 = (TextView) findViewById(R.id.bignum);
+            newTextView1.setText("" + MakePayment.BigBenBinNum);
         }
 
-        if(MakePayment.EssentialsBinNum>=5){
-            TextView newTextView=(TextView) findViewById(R.id.essnum);
+        if (MakePayment.EssentialsBinNum >= 5) {
+            TextView newTextView = (TextView) findViewById(R.id.essnum);
             newTextView.setText("Full");
-        }
-        else{
-            TextView newTextView2=(TextView) findViewById(R.id.essnum);
-            newTextView2.setText(""+MakePayment.EssentialsBinNum);
+            sendEmail("Essentials");
+        } else {
+            TextView newTextView2 = (TextView) findViewById(R.id.essnum);
+            newTextView2.setText("" + MakePayment.EssentialsBinNum);
         }
     }
-    public native String getAkornoBin();
-    public native String getBigbenBin();
-    public native String getEssentialsBin();
+
+    public void sendEmail(String location) {
+        Log.i("Send email", "");
+
+        String[] TO = {"issifu.alhassan@ashesi.edu.gh"};
+        String[] CC = {"faweenagua@gmail.com"};
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+
+
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        emailIntent.putExtra(Intent.EXTRA_CC, CC);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Bin at "+location+" is full");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Hi Manager, \n the bin placed at "+location+" is filled to its capacity. " +
+                "Five bottles have been placed there hence you should be ready to pay GHc 1.00 upon collection\n\n Best regards\n Repsyche.");
+
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+            finish();
+            Log.i("Mail sent...", "");
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(Zoomlion.this,
+                    "There is no email client installed.", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
